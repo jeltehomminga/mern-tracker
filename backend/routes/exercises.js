cda2e99cefffb747d1737e788d670808dfce3d68
Jelte Homminga <jeltehomminga@gmail.com>
2019-05-26 00:10:10 +0200

    completed exercise routes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -9,11 +9,40 @@ router.get('/', (req, res) => {
 })
 
 router.post('/add', (req, res, next) => {
+  const username = req.body.username
+  const description = req.body.description
+  const duration = Number(req.body.duration)
+  const date = Date.parse(req.body.date)
+
   const newExcercise = new Excercise({ username, description, duration, date })
 
   newExcercise
     .save()
     .then(() => res.json('Excercise added!'))
+    .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+router.get('/:id', (req, res) => {
+  Excercise.findById(req.params.id)
+    .then(excercise => res.json(excercise))
+    .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+router.delete('/:id', (req, res) => {
+  Excercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('exercise deleted'))
+    .catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+router.post('/update/:id', (req, res) => {
+  const update = {
+    username: req.body.username,
+    description: req.body.description,
+    duration: Number(req.body.duration),
+    date: Date.parse(req.body.date)
+  }
+  Excercise.findByIdAndUpdate(req.params.id, update)
+    .then(() => res.json('Exercise updated!'))
     .catch(err => res.status(400).json(`Error: ${err}`))
 })
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
@@ -11,10 +11,14 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
 
 const connection = mongoose.connection
 
